7c9427350f81879ed3c7f5701ec4ed3de2d856de
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-04-04 14:22:19 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import favicon from './favicon.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={favicon} className="App-logo" alt="logo" />
+            <h1 className="App-title">Fibonaaci Calculator on Kubernetes</h1>
+            <Link to="/">Home</Link><br/>
+            <Link to="/otherpage">Description</Link>
+          </header>
+          <br />
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/OtherPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <p>I have a database on <strong>Postgres</strong> which stores all the indices and its corresponding values that i have calculated.</p>
+      <br/>
+      <p>Also I have a <strong>Redis Server</strong> which stores all the indices to which i have calculated the values for.</p>
+      <br />
+      <Link to="/">FiboKube Home</Link>
+    </div>
+  );
+};
