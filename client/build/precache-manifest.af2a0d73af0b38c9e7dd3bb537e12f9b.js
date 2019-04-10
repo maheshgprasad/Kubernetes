6b6bf886af23cf30fe83b00abebfa41bf2db51b8
Mahesh G Prasad <42564944+maheshgprasad@users.noreply.github.com>
2019-04-10 16:06:45 +0530

    added Build

---
+++ client/build/precache-manifest.af2a0d73af0b38c9e7dd3bb537e12f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e47d806e5cc141b960de8f4f20e120f",
+    "url": "/static/media/favicon.1e47d806.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1f430db1be9e6b2cd912",
+    "url": "/static/js/main.6cd50022.chunk.js"
+  },
+  {
+    "revision": "9c78aee383db3962c857",
+    "url": "/static/js/2.3cd16667.chunk.js"
+  },
+  {
+    "revision": "1f430db1be9e6b2cd912",
+    "url": "/static/css/main.a355ee22.chunk.css"
+  },
+  {
+    "revision": "423510601304577abd753abcdec1b16b",
+    "url": "/index.html"
+  }
+];
